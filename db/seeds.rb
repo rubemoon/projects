a4ecc740b4829031efe193d3743ab325144af418
Rubens Jean Simon <93041872+rubemoon@users.noreply.github.com>
2024-07-22 02:24:06 +0000

    add some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@
 
 
 PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word }
+
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.length * SPLIT
+
+PROJECT_USERS = {
+    'me@example.com' => PROJECT_TITLES[0, OFFSET],
+    'you@example.com' => PROJECT_TITLES[OFFSET..-1]
+}
+
+PROJECT_USERS.each do |email, project|
+    user = User.new(email: email, password: 'password')
+    project.each { |title| user.projects.build(title: title) }
+    user.save
+end
